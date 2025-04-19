5d1031eb53e80c6aed0cad27da2d13bd08a853f3
nina <ninaw2306@gmail.com>
2025-04-19 21:47:22 +0800

    chore: 設定 baseURL

--- vite.config.mjs
+++ vite.config.mjs
@@ -10,6 +10,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Vuetify-todos/',
   plugins: [
     Vue({
       template: { transformAssetUrls },
